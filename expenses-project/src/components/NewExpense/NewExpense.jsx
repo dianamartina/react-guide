3b7e39a8e483ba323889c029221c119173c3dac4
dianamartina <dianamartina000@gmail.com>
2021-10-06 20:56:45 +0300

    expenses project

---
+++ expenses-project/src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = { ...enteredExpenseData, id: Math.random().toString() };
+    console.log(expenseData);
+
+    props.onAddExpense(expenseData);
+    setIsEditing(false); // the form is closed after submit
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditing = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditing}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
